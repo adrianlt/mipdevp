84e5d38b4428476ce20f1e8ef37b9b5655156eb5
neowise77 <adrianbeltsazar@gmail.com>
2022-09-14 15:21:02 +0700

    youtube embed video script added 3

--- assets/js/youtube-defer.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  var hndl = document.getElementsByTagName('iframe');
-  for (var i = 0; i < hndl.length; i++) {
-    if (hndl[i].getAttribute('data-src')) {
-      hndl[i].setAttribute('src', hndl[i].getAttribute('data-src'));
-    }
-  }
-}
